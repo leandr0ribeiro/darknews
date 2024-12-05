c9cd216c5de7ebbcede85ae7485897cc8db014e1
Leandro Ribeiro <souza.ribeiro92@gmail.com>
2024-12-05 21:19:40 +0000

    edit `orchestrator.js`

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -14,7 +14,12 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+
+      console.log(response.status);
+
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
 }
