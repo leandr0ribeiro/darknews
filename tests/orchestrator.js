f698b7df109c167ef2208e576196e497433e4df1
Leandro Ribeiro <souza.ribeiro92@gmail.com>
2025-05-08 20:25:36 +0000

    style: change format

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,8 +1,13 @@
 import retry from "async-retry";
 import database from "infra/database.js";
+import migrator from "models/migrator.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
+}
+
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
 }
 
 async function clearDatabase() {
@@ -27,6 +32,7 @@ async function fetchStatusPage() {
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
+  runPendingMigrations,
 };
 
 export default orchestrator;
